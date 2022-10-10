69e56855fe38064a95b87f878149acd4d58cdf06
Vadim Tsvetkov <florius0@ninsar.pro>
2022-10-10 04:43:57 +0300

    Run checker only if source extraction was successful

--- check.sh
+++ check.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 
-python3 buildscripts/sources.py $(find chapters -name '*.md') -o sources/
+python3 buildscripts/sources.py $(find chapters -name '*.md') -o sources/ &&\
 python3 buildscripts/check.py sources-metadata.yaml -p sources/
 
